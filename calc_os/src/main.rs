bf5894d5ad06788627b41277c00dbb26640e3b00
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-23 21:33:17 +0200

    calc_os_create

---
+++ calc_os/src/main.rs
@@ -0,0 +1,15 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
