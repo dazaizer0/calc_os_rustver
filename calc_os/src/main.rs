2685dc907bc186f41d07fe0eea3050dfd9024140
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-24 17:42:45 +0200

    Update main.rs

--- calc_os/src/main.rs
+++ calc_os/src/main.rs
@@ -1,22 +1,21 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner)]
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"HelloWorld";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[cfg(test)]
+fn test_runner(_test: &[&dyn Fn()]) {
     loop {}
 }
