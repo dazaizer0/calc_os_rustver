0e3fb1dfa34c2ba5bc3a81582c6921ae1c9e9af1
dazai <60112867+dazaizer0@users.noreply.github.com>
2023-09-25 14:32:31 +0200

    omg

--- calc_os/src/main.rs
+++ calc_os/src/main.rs
@@ -2,18 +2,11 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
 
